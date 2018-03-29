7d2fb3a3416d62e7cca13f4c7b760051fe92531a
Dimetriu <dima.rashidov@gmail.com>
2018-03-29 16:11:46 +0300

    add omniauth callbacks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  devise_for :users
+  devise_for :users, controllers: { omiauth_callbacks: 'users/omniauth_callbacks' }
   
   devise_for :admins
 
